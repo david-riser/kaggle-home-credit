65ae85130977691e17fe4b5cf4e2c80d96f9e44c
David Riser <dmriser@jlab.org>
2018-07-17 09:50:27 -0400

    Added xgboost classifier.  Continue to have problems with the sklearn models changing the shape of training and testing arrays.

--- src/models/1.0-train-xgboost.py
+++ src/models/1.0-train-xgboost.py
@@ -18,7 +18,7 @@ def main():
     # Define constants and configurations.
     path_to_data   = '../../data/processed/'
     path_to_output = '../../data/submissions/'
-    sample_size    = 10000
+    sample_size    = None
     SEED           = 8675309
 
     # Load both training and testing
@@ -46,6 +46,9 @@ def main():
         x_train, y_train = train.iloc[train_index].values, labels[train_index]
         x_valid, y_valid = train.iloc[val_index].values, labels[val_index]
 
+        #        x_train.columns = train.columns
+        #        x_valid.columns = train.columns
+
         clf = xgb.XGBClassifier(
             learning_rate=0.1,
             n_estimators=10000,
@@ -61,17 +64,17 @@ def main():
         )
 
         clf.fit(x_train, y_train, eval_set=[(x_train, y_train), (x_valid, y_valid)], eval_metric='auc',
-                verbose=20, early_stopping_rounds=200)
+                verbose=20, early_stopping_rounds=50)
 
-        oof_preds[val_index] = clf.predict_proba(x_valid)[:,1]
-        sub_preds += clf.predict_proba(test)[:,1] / n_folds
+        oof_preds[val_index] = clf.predict_proba(x_valid, ntree_limit=clf.best_ntree_limit)[:,1]
+        sub_preds += clf.predict_proba(test.values, ntree_limit=clf.best_ntree_limit)[:,1] / n_folds
 
     # Predict the test dataset 
     print('Total ROC AUC = %.4f' % roc_auc_score(labels, oof_preds))
 
     submission = pd.DataFrame({'SK_ID_CURR':test_ids, 'TARGET':sub_preds})
     submission['SK_ID_CURR'] = submission['SK_ID_CURR'].astype('int32')
-    submission.to_csv(path_to_output+'1.1-lightgbm.csv', index=False)
+    submission.to_csv(path_to_output+'1.1-xgboost.csv', index=False)
 
 if __name__ == '__main__':
     main()
